583b32ed16170a55ae0fad6a68cd3bce23d015cf
Qomaraawae <141138947+Qomaraawae@users.noreply.github.com>
2025-01-27 22:16:13 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
+    <footer className="bg-gray-900 text-white py-8 mt-auto">
+      {" "}
+      {/* Tambahkan mt-auto */}
       <div className="max-w-6xl mx-auto px-4 text-center">
         <p>© 2024 Cafe Teras Omah. All Rights Reserved.</p>
       </div>
