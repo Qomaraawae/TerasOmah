adf22fc729f09fe1fbe391c63b04a2712a4608e3
Qomaraawae <141138947+Qomaraawae@users.noreply.github.com>
2025-01-28 16:38:38 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/some-library/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require("@tailwindcss/typography"), // Plugin untuk styling teks
+    require("@tailwindcss/forms"), // Plugin untuk form styling
+  ],
+};
